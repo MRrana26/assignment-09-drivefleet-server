471e13d1b65e1a6698e512d31f6cf564f2748fe5
MRrana26 <hafezmasudranamn@gmail.com>
2026-05-21 17:41:54 +0600

    jwt token verification

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI;
@@ -19,19 +20,32 @@ const client = new MongoClient(uri, {
   }
 });
 
-const verifyToken = (req, res, next) => {
-      const authHeader = req?.headers.authorization
-      if(!authHeader){
-        return res.status(401).json({message: "Unauthorized"});
-      }
-      const token = authHeader.split(" ")[1]
-      if(!token){
-        return res.status(401).json({message: "Unauthorized"});
-      }
-      
-      next()
+const JWKS = createRemoteJWKSet(
+  new URL("http://localhost:3000/api/auth/jwks")
+)
 
-    }
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1]
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS)
+    console.log(payload)
+    next()
+  } catch (error) {
+    return res.status(403).json({message:  "Forbidden"
+    });
+  }
+
+  
+
+}
 
 async function run() {
   try {
@@ -47,11 +61,11 @@ async function run() {
 
     })
 
-    app.get("/available-cars/:id", verifyToken, async (req, res)=> {
-      const {id} = req.params
-      const result = await availableCarsCollection.findOne({_id: new ObjectId(id)})
+    app.get("/available-cars/:id", verifyToken, async (req, res) => {
+      const { id } = req.params
+      const result = await availableCarsCollection.findOne({ _id: new ObjectId(id) })
       res.json(result);
-        })
+    })
 
 
     app.get('/available-cars', async (req, res) => {
